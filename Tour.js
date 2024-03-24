cc94bc22ed3b9daf3a621f8843beed65cf4e66fe
samarthtiwar <109853989+samarthtiwar@users.noreply.github.com>
2024-03-24 16:42:18 +0530

    Add files via upload

---
+++ Tour.js
@@ -0,0 +1,50 @@
+AFRAME.registerComponent("tour", {
+  init: function () {
+    this.placesContainer = this.el;
+    
+  },
+
+  createCards: function () {
+    const thumbNailsRef = [
+      {
+        id: "taj-mahal",
+        title: "Taj Mahal",
+        url: "./assets/thumbnails/taj_mahal.png",
+      },
+      {
+        id: "budapest",
+        title: "Budapest",
+        url: "./assets/thumbnails/budapest.jpg",
+      },
+
+      {
+        id: "eiffel-tower",
+        title: "Eiffel Tower",
+        url: "./assets/thumbnails/eiffel_tower.jpg",
+      },
+      {
+        id: "new-york-city",
+        title: "New York City",
+        url: "./assets/thumbnails/new_york_city.png",
+      },
+    ];
+    let prevoiusXPosition = -60;
+
+    for (var item of thumbNailsRef) {
+      const posX = prevoiusXPosition + 25;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      
+      // Thumbnail Element
+     
+      // Title Text Element
+      
+      this.placesContainer.appendChild(borderEl);
+    }
+  },
+  
+});
